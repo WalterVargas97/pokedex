097eb3e7883bf2c4ad4e4117e6bbf8062ade023a
wally1997 <walterbolivar97@gmail.com>
2022-12-06 22:58:25 -0500

    api enlazada con header y comienzo buscador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokelistComponent } from './pokelist/pokelist.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BuscadorComponent } from './buscador/buscador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokelistComponent,
+    HeaderComponent,
+    FooterComponent,
+    BuscadorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pokelist/pokelist.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { AppiService } from '../appi.service';
+
+
+@Component({
+  selector: 'app-pokelist',
+  templateUrl: './pokelist.component.html',
+  styleUrls: ['./pokelist.component.sass']
+})
+export class PokelistComponent {
+ 
+  pokemons:any[]=[]
+
+  constructor(
+    private appiservice: AppiService
+  ){}
+  ngOnInit():void{
+    this.appiservice.getPokemons()
+    .subscribe((response:any)=>{
+      /* this.totalPokemons=response.count; */
+
+      response.results.forEach((result: { name: string; }) => {
+        this.appiservice.getMoreData(result.name)
+        .subscribe((response:any)=>{
+          /* this.pokemons.push(response); */
+          let type='';
+    
+          response.types.forEach((element: any) => {
+            type=type+element.type.name.charAt(0).toUpperCase()+element.type.name.slice(1)+' '
+          });
+
+          this.pokemons.push({
+                'name':response.name.charAt(0).toUpperCase()+response.name.slice(1),
+                'img':response.sprites.front_default,
+              'height':response.height,
+              'health':response.stats[0].base_stat,
+              'atack':response.stats[1].base_stat,
+              'types':type});
+        
+
+        })
+      });
+    });
+  }
+}
